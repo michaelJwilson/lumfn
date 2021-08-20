41a08a350165c79c518d94c1d3ef06fb28d962a0
michaelJwilson <mjwilson@lbl.gov>
2021-08-20 06:21:16 -0700

    Updates.

--- py/lumfn/SV3/gen_cats.py
+++ py/lumfn/SV3/gen_cats.py
@@ -10,6 +10,7 @@ from   astropy.table     import Table, join
 from   desitarget.sv3    import sv3_targetmask
 from   matplotlib.pyplot import figure
 from   scipy.optimize    import curve_fit
+from   zsuccess          import zsuccess
 
 sys.path.append('/global/homes/m/mjwilson/desi/BGS/lumfn/bin')
 sys.path.append('/global/homes/m/mjwilson/desi/BGS/lumfn/py/')
@@ -24,8 +25,6 @@ from   abs_mag           import abs_mag
 from   vmax              import zmax, vmax
 from   LSS.SV3.cattools  import tile2rosette  
 
-def zsuccess(rfiber, a=2.68, b=22.113):
-    return 1. /  (1. + np.exp(a * (rfiber - b)))
 
 odir             = os.environ['CSCRATCH'] + '/desi/BGS/lumfn/'
 
@@ -42,7 +41,8 @@ bright_merge     = bright_merge[(sv3_targetmask.bgs_mask['BGS_BRIGHT'] & bright_
 
 # Limit to spectro. observations on a working fiber.
 # Limit to first observation.  Subsequent observations reassigned on the basis of a bad first redshift.  Check: is 3000 true only of dark time?  Doubtful. 
-bright_merge_obs                    = bright_merge[(bright_merge['ZWARN'].data != 999999) & (bright_merge['FIBERSTATUS'] == 0) & (bright_merge['PRIORITY'] > 3000.)]
+# 300 km/s lower limit for stars; upper limit due to extrapolation of k correction.
+bright_merge_obs                    = bright_merge[(bright_merge['ZWARN'].data != 999999) & (bright_merge['FIBERSTATUS'] == 0) & (bright_merge['PRIORITY'] > 3000.) & (bright_merge['Z'] >= 0.001) & (bright_merge['Z'] <= 0.5)]
 
 # Add fluxes & colors. 
 bright_merge_obs                    = join(bright_merge_obs, bgs_bright['FIBERFLUX_R', 'FLUX_G', 'FLUX_R', 'MW_TRANSMISSION_G', 'MW_TRANSMISSION_R', 'TARGETID'], join_type='left', keys='TARGETID')
@@ -62,17 +62,16 @@ bright_merge_obs['GMR_DRED']        = bright_merge_obs['GMAG_DRED'] - bright_mer
 bright_merge_obs['BGS_Z_SUCCESS']   = (bright_merge_obs['ZWARN'] == 0) & (bright_merge_obs['DELTACHI2'] > 40.)
 bright_merge_obs['BGS_Z_WEIGHT']    = 1. / zsuccess(bright_merge_obs['FIBER_RMAG'])
 
-# 300 km/s lower limit for stars; upper limit due to extrapolation of k correction. 
-bright_merge_cat                    = bright_merge_obs[(bright_merge_obs['Z'] >= 0.001) & (bright_merge_obs['Z'] <= 0.5)]
-bright_merge_cat.write('{}bright_sv3.fits'.format(odir), format='fits', overwrite=True)
+bright_merge_obs.write('{}bright_sv3.fits'.format(odir), format='fits', overwrite=True)
 
+'''
 derived     = []
 
 start       = time.time()
 
 kcorrector  = ajs_kcorr()
 
-for ii, row in enumerate(bright_merge_cat):
+for ii, row in enumerate(bright_merge_obs):
     tid     = row['TARGETID']
 
     ros     = tile2rosette(row['ZTILEID'])
@@ -101,15 +100,10 @@ for ii, row in enumerate(bright_merge_cat):
         percentage_complete = 100. * ii / len(bright_merge_cat)
         
         print('{:.2f} complete after {:.2f} minutes.'.format(percentage_complete, runtime))
-        '''
-        if runtime > .5:
-            break
-        '''
         
-derived = Table(np.array(derived), names=['TARGETID', 'ROSETTE', 'BGSZWEIGHT', 'VOLUME', 'MRH', 'ZMAX', 'VMAX', 'VONVMAX'])
-
-derived.pprint(max_width=-1)
-
-derived.write('{}bright_sv3_derived.fits'.format(odir), format='fits', overwrite=True)
+# derived = Table(np.array(derived), names=['TARGETID', 'ROSETTE', 'BGSZWEIGHT', 'VOLUME', 'MRH', 'ZMAX', 'VMAX', 'VONVMAX'])
+# derived.pprint(max_width=-1)
+# derived.write('{}bright_sv3_derived.fits'.format(odir), format='fits', overwrite=True)
 
 print('Finished writing to' + ' {}bright_sv3_derived.fits'.format(odir))
+'''
---
+++ py/lumfn/SV3/zweight_coeff.py
@@ -0,0 +1,44 @@
+import numpy             as     np
+import pylab             as     pl
+import astropy.io.fits   as     fits
+
+from   astropy.table     import Table
+from   matplotlib.pyplot import figure
+from   scipy.optimize    import curve_fit
+from   matplotlib.pyplot import figure
+from   zsuccess          import zsuccess
+
+
+figure(figsize=(6, 4), dpi=200)
+
+bright = Table.read('/global/cscratch1/sd/mjwilson/desi/BGS/lumfn/bright_sv3.fits')
+
+print(bright.dtype.names)
+
+bright['RMAG_IDX'] = np.digitize(bright['FIBER_RMAG'], bins=np.arange(19.5, 22., 0.2))
+
+bright_grouped = bright.group_by(['RMAG_IDX'])
+bright_binned  = bright_grouped['TSNR2_BGS', 'FIBER_RMAG', 'RMAG', 'RMAG_DRED', 'RMAG_IDX', 'BGS_Z_SUCCESS'].groups.aggregate(np.mean)
+
+bright_binned.sort('FIBER_RMAG')
+
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
+popt, pcov = curve_fit(zsuccess, bright_binned['FIBER_RMAG'], bright_binned['BGS_Z_SUCCESS'], p0=[1.31, 22.3])
+
+print(popt)
+
+## 
+xs = np.arange(19.0, 23.0, 0.05)
+
+pl.plot(bright_binned['FIBER_RMAG'], bright_binned['BGS_Z_SUCCESS'], c='k', lw=0.25, label='BGS BRIGHT')
+pl.plot(xs, zsuccess(xs), label='Best fit', alpha=0.5)
+pl.plot(xs, zsuccess(xs, a=1.31, b=22.314), label='Sam', alpha=0.5)
+
+pl.xlabel('FIBER_RMAG')
+pl.ylabel('BGS $z$ Success')
+
+pl.legend(frameon=False, loc=1)
+
+pl.ylim(-0.05, 1.05)
+
+pl.show()
