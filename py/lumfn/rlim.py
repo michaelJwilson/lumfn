83a23521007f6b51c2ca6f6d074c8f6fd261c729
michaelJwilson <mjwilson@lbl.gov>
2021-08-26 00:44:19 -0700

    Add github link to rlim code.

--- py/lumfn/rlim.py
+++ py/lumfn/rlim.py
@@ -1,6 +1,7 @@
 from params import params
 
 def rlim(photsys, debug=True):
+    # https://github.com/desihub/desitarget/blob/19031803b56f83898a99112c02e8e5d671069c28/py/desitarget/sv3/sv3_cuts.py#L1358
     nom    = params['rlim']
     offset = 0.04
 
